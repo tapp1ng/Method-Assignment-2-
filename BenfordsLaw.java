ab7aeb0133f68472f0e1623a6a38aa3b9890dba5
Carl <335563110@gapps.yrdsb.ca>
2021-05-07 11:52:33 -0400

    Updated for clarity

--- BenfordsLaw.java
+++ BenfordsLaw.java
@@ -14,7 +14,22 @@ class BenfordsLaw{
         int userMenuChoice = 0; // User selected option in the menu
         boolean exit = false;
         int[] sales = new int[0]; // --> Need a plan to get this to my method analyzeData
+
         double[] digitFreq = new double[10];
+        /*
+        [0] - digit freq of 0
+        [1] - digit freq of 1
+        [2] - digit freq of 2
+        [3] - digit freq of 3
+        [4] - digit freq of 4
+        [5] - digit freq of 5
+        [6] - digit freq of 6
+        [7] - digit freq of 7
+        [8] - digit freq of 8
+        [9] - digit freq of 9
+        */
+        
+        boolean loadedSales = false; // --> For your load method, do you think you can have it return true once the user loads data into array?
 
         while (!exit){
             printMenu();
@@ -27,7 +42,12 @@ class BenfordsLaw{
                 // method here
             }
             else if (userMenuChoice == 4){ // Check for fruad and generate graph
-                analyzeData(digitFreq, sales);
+                if (!loadedSales){
+                    print("Please load sales data first!");
+                }
+                else{
+                    analyzeData(digitFreq, sales);
+                }
             }
             else if (userMenuChoice == 5){ // Export digit frequency in .csv
                 // method here
@@ -57,7 +77,7 @@ class BenfordsLaw{
 
     /*
      * Author: Carl Wang
-     * Method prints out an array of values - INTEGER ARRAY
+     * Method prints out an array of values - INTEGER ARRAY -------------------------------- May get rid of -----------------------------------
      * 
      * @param int[] arr - Integer array with data
      * */
@@ -71,7 +91,8 @@ class BenfordsLaw{
      * Author: Carl Wang
      * The "main base of operations" for analyzing the sales data
      * 
-     * @param ------------------------------------------------------------
+     * @param double[] digitFreq - Contains the digit frequency of sales data
+     * @param int[] sales - Contains the sales data
      * */
     public static void analyzeData (double[] digitFreq, int[] sales){
 
